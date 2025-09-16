3fd1d4e98bba21d76c9a6aea991453ffeb97feb7
Connor Geraghty <123890489+ConnorGeraghty1@users.noreply.github.com>
2025-09-16 09:36:43 -0400

    init background.js

---
+++ Arduino/Chrome Extension/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { //listens for incoming messages
+    if (message.type === "sendToHost") {
+        // The 'data' field contains the tactile command string
+        const tactileCommand = message.data;
+
+        // Send the tactileCommand to the native application via native messaging
+        chrome.runtime.sendNativeMessage("com.yourcompany.nativehost", { command: tactileCommand }, (response) => {
+            if (chrome.runtime.lastError) {
+                console.error("Error communicating with native host:", chrome.runtime.lastError);
+            } else {
+                console.log("Native host response:", response);
+            }
+        });
+    }
+});
